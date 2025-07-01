f0ac3abd6aed5cdc4eaf6f00125293b579183fd0
tarunbharti7 <tarunbharti970@gmail.com>
2025-07-01 19:13:33 +0530

    v3

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://todo-mern-ia3f.onrender.com//user/login", {
+      const response = await axios.post("https://todo-mern-ia3f.onrender.com/user/login", {
         email,
         password,
       });
